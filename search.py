5549e8dfaf563cf1eca946b538d169198023eba2
YulinXing <50935890+YulinXing@users.noreply.github.com>
2019-07-01 15:21:27 +0800

    Update search.py

--- search.py
+++ search.py
@@ -1,30 +1,22 @@
-def search_linear(alist, item):
+def search(alist, item):
     
     for i in range(len(alist)):
         if item == alist[i]:
             return (i)
     return -1
 
-def find_unknown_words(vocab, wds):
+def unknownWords(vocab, wds):
     result = []
     for w in wds:
         if (search_linear(vocab, w) < 0):
             result.append(w)
     return result
 
-def load_words_from_file(filename):
+def loadWords(filename):
     f = open(filename, "r")
     file_content = f.read()
     f.close()
     wds = file_content.split()
-    return wds
-
-def text_to_words(the_text):
-    my_substitutions = the_text.maketrans(
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&()*+,-./:;<=>?@[]^_`{|}~'\\",
-      "abcdefghijklmnopqrstuvwxyz                                          ")
-    cleaned_text = the_text.translate(my_substitutions)
-    wds = cleaned_text.split()
     return wds
 
 def get_words_in_book(filename):
@@ -46,12 +38,5 @@ book_words = get_words_in_book("AliceInWonderland.txt")
 print("There are {0} words in the book, the first 100 are\n{1}".
            format(len(book_words), book_words[:100]))
 
-
-
-import time
-
-t0 = time.clock()
 missing_words = find_unknown_words(vocab, book_words)
-t1 = time.clock()
 print("There are {0} unknown words.".format(len(missing_words)))
-print("That took {0:.4f} seconds.".format(t1-t0))
