487e635944fcd6b8c1af66a408eb30abfd878f84
YulinXing <50935890+YulinXing@users.noreply.github.com>
2019-09-02 15:43:03 +0800

    Update 2d_array.py

--- 2d_array.py
+++ 2d_array.py
@@ -9,28 +9,25 @@ arr_initial=[[80,80,255,80,80,255,80,80],
 
 def task1(arr):
 	burnt_out=False
-    for i in range(len(arr)):
-        for x in range(len(arr[i])):
-            arr[i][x]*=1.1
-            if arr[i][x]==255:
-                burnt_out=True
-                
-    if burnt_out==False:
-        return arr
-    else:
-        return True
+	for i in range(len(arr)):
+		for x in range(len(arr[i])):
+			arr[i][x]*=1.1
+			if arr[i][x]==255
+			burnt_out=True
+	if burnt_out==False:
+        	return arr
+    	else:
+       		return True
  
 def task2(arr):
-    for i in range(len(arr)):
-        arr[i]=arr[i][::-1]
-    return arr
+	for i in range(len(arr)):
+		arr[i]=arr[i][::-1]
+		return arr
 
- def task3(arr):
- 	MaxVal=int(input("Max value:"))
- 	for i in range(len(arr)):
- 		for x in range(len(arr[i])):
- 			if arr[i][x]>MaxVal:
- 				arr[i][x]=MaxVal
- 	return arr
-
-print(task1(arr_initial))
+def task3(arr):
+	MaxVal=int(input("Max value:"))
+	for i in range(len(arr)):
+		for x in range(len(arr[i])):
+			if arr[i][x]>MaxVal:
+				arr[i][x]=MaxVal
+	return arr
