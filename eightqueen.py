87be5d8166fcd33e9ffff7322ab004c5ec6d05b4
YulinXing <50935890+YulinXing@users.noreply.github.com>
2019-09-27 12:36:47 +0800

    Add files via upload

---
+++ eightqueen.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 13:50:43 2019
+
+@author: timxing
+"""
+
+def share_diagonal(x0, y0, x1, y1):
+    dy = abs(y1 - y0)        
+    dx = abs(x1 - x0)        
+    return dx == dy 
+
+def col_clashes(bs, c):
+    for i in range(c):   
+          if share_diagonal(i, bs[i], c, bs[c]):
+              return True
+    return False      
+
+def has_clashes(the_board):
+    for col in range(1,len(the_board)):
+        if col_clashes(the_board, col):
+            return True
+    return False
+
+def main():
+    import random
+    rng = random.Random()   
+    bd = list(range(8))     
+    num_found = 0
+    tries = 0
+    while num_found < 92:
+       rng.shuffle(bd)
+       tries += 1
+       if not has_clashes(bd):
+           print("Found solution {0} in {1} tries.".format(bd, tries))
+           tries = 0
+           num_found += 1
+     
+main()
